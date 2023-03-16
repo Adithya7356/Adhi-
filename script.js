fe35f3ae7809342c162b82cf2cb113814a8c831b
adithya7356 <adithyanrmeladoor2255@gmail.com>
2023-03-16 04:26:54 -0700

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    let sum = a+b;
+   console.log(sum)
+
+}
+//add(2,4)
+
+
+const add1 = (a,b)=>{
+    console.log(a+b);
+
+}
+add1(4,4);
+
+const mult1 =(a,b)=>{
+    console.log(a*b);
+
+}
+mult1(5,5)
